c4f79712d74af60bb94b6a26728bc0668674ee65
rulamon <ruben.lamon@gmail.com>
2018-12-11 14:44:54 +0100

    replaced str with val in decipher function to avoid weird stuff

--- 2.js
+++ 2.js
@@ -1,15 +1,15 @@
 function rot13(str) { // LBH QVQ VG!
-	const decipher = (str) => {
-		let code = str.charCodeAt(0);
+	const decipher = (val) => {
+		let code = val.charCodeAt(0);
 		if (code >= 65 && code <= 90) {
 			return code < 78 ? String.fromCharCode(code + 13) : String.fromCharCode(code - 13);
 		} else {
-			return str;
+			return val;
 		}
 	}
 	let solution = "";
 	for (let i = 0; i < str.length; i++) {
-		solution += decipher(str[i])
+		solution += decipher(str[i]);
 	}
   return solution;
 }
